088a15a4591c1d6d020225b76d4208bcf9a8e8e5
wildernessnz <systems@wilderness.co.nz>
2026-05-06 11:39:39 +1200

    Delete old

--- active/rata-bookings-analysis/WildernessAppScriptLibrary.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace WildernessAppScriptLibrary {
-  class Utility {
-    isInWorkingHours(startHour: number, endHour: number): boolean;
-  }
-
-  function getSharedDataclipUrl(dataclipId: string, sheetName: string): string;
-}
